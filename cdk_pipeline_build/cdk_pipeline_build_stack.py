dd2bc279a395079126c28cce3882bfe8c3cf64c0
w601sxs <shreyas.f117@gmail.com>
2020-12-22 17:01:55 -0500

    Update cdk_pipeline_build_stack.py

--- cdk_pipeline_build/cdk_pipeline_build_stack.py
+++ cdk_pipeline_build/cdk_pipeline_build_stack.py
@@ -6,8 +6,8 @@ import aws_cdk.aws_codepipeline as codepipeline
 import aws_cdk.aws_codepipeline_actions as codepipeline_actions
 
 class CdkPipelineBuildStack(Stack):
-    def __init__(self, scope: Construct, id: str, * * kwargs) - > None:
-        super().__init__(scope, id, * * kwargs)
+    def __init__(self, scope: Construct, id: str, **kwargs) - > None:
+        super().__init__(scope, id, **kwargs)
         source_artifact = codepipeline.Artifact()
         cloud_assembly_artifact = codepipeline.Artifact()
         bucket = _s3.Bucket(self, 'bucket')
