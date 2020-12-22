487ac1b1aeb9d21b62810a67090ae4b16c4f6d7b
w601sxs <shreyas.f117@gmail.com>
2020-12-22 15:49:36 -0500

    Update cdk_pipeline_build_stack.py

--- cdk_pipeline_build/cdk_pipeline_build_stack.py
+++ cdk_pipeline_build/cdk_pipeline_build_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk.core import Stack, StackProps, Construct, SecretValue
 from aws_cdk.pipelines import CdkPipeline, SimpleSynthAction
+import aws_cdk.aws_s3 as _s3
 
 import aws_cdk.aws_codepipeline as codepipeline
 import aws_cdk.aws_codepipeline_actions as codepipeline_actions
@@ -11,24 +12,25 @@ class CdkPipelineBuildStack(Stack):
 
         source_artifact = codepipeline.Artifact()
         cloud_assembly_artifact = codepipeline.Artifact()
-
-        pipeline = CdkPipeline(self, "Pipeline",
+	bucket = _s3.Bucket(self,'bucket')
+        
+	pipeline = CdkPipeline(self, "Pipeline",
             pipeline_name="MyAppPipeline",
             cloud_assembly_artifact=cloud_assembly_artifact,
-# 	      source_action=codepipeline_actions.S3SourceAction(
-# 		  bucket = "faropt-us-east-1",
-# 		  bucket_key="faropt-master.zip",
-# 		  action_name="GitHub",
-# 		  output=source_artifact),		  
-            source_action=codepipeline_actions.GitHubSourceAction(
-		oauth_token=SecretValue(value="None"),
-                action_name="GitHub",
-                output=source_artifact,
-                trigger=codepipeline_actions.GitHubTrigger.POLL,
-                # Replace these with your actual GitHub project info
-                owner="aws-samples",
-                repo="faropt"),
-		synth_action=SimpleSynthAction.standard_npm_synth(
+	      source_action=codepipeline_actions.S3SourceAction(
+		  bucket = bucket.bucket_name,
+		  bucket_key="faropt-master.zip",
+		  action_name="S3",
+		  output=source_artifact),		  
+#             source_action=codepipeline_actions.GitHubSourceAction(
+# 		oauth_token=SecretValue(value="None"),
+#                 action_name="GitHub",
+#                 output=source_artifact,
+#                 trigger=codepipeline_actions.GitHubTrigger.POLL,
+#                 # Replace these with your actual GitHub project info
+#                 owner="aws-samples",
+#                 repo="faropt"),
+	    synth_action=SimpleSynthAction.standard_npm_synth(
                 source_artifact=source_artifact,
                 cloud_assembly_artifact=cloud_assembly_artifact,
                 # Use this if you need a build step (if you're not using ts-node
