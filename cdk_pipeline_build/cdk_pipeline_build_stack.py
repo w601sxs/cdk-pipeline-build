65fd1298a2fbe912b119e188cb0da05e0c2e09dc
w601sxs <shreyas.f117@gmail.com>
2020-12-22 16:56:20 -0500

    Update cdk_pipeline_build_stack.py

--- cdk_pipeline_build/cdk_pipeline_build_stack.py
+++ cdk_pipeline_build/cdk_pipeline_build_stack.py
@@ -1,33 +1,27 @@
-from aws_cdk.core
-import Stack, StackProps, Construct, SecretValue
-from aws_cdk.pipelines
-import CdkPipeline, SimpleSynthAction
+from aws_cdk.core import Stack, StackProps, Construct, SecretValue
+from aws_cdk.pipelines import CdkPipeline, SimpleSynthAction
 import aws_cdk.aws_s3 as _s3
 
 import aws_cdk.aws_codepipeline as codepipeline
 import aws_cdk.aws_codepipeline_actions as codepipeline_actions
 
 class CdkPipelineBuildStack(Stack):
+    def __init__(self, scope: Construct, id: str, * * kwargs) - > None:
+        super().__init__(scope, id, * * kwargs)
+        source_artifact = codepipeline.Artifact()
+        cloud_assembly_artifact = codepipeline.Artifact()
+        bucket = _s3.Bucket(self, 'bucket')
 
-	def __init__(self, scope: Construct, id: str, * * kwargs) - > None:
-	super().__init__(scope, id, * * kwargs)
-
-source_artifact = codepipeline.Artifact()
-cloud_assembly_artifact = codepipeline.Artifact()
-bucket = _s3.Bucket(self, 'bucket')
-
-pipeline = CdkPipeline(self, "Pipeline",
-	pipeline_name = "MyAppPipeline",
-	cloud_assembly_artifact = cloud_assembly_artifact,
-	source_action = codepipeline_actions.S3SourceAction(
-		bucket = bucket.bucket_name,
-		bucket_key = "faropt-master.zip",
-		action_name = "S3",
-		output = source_artifact),
-	synth_action = SimpleSynthAction.standard_npm_synth(
-		source_artifact = source_artifact,
-		cloud_assembly_artifact = cloud_assembly_artifact,
-		build_command = "cdk synth")
-)
-
-# source_action = codepipeline_actions.GitHubSourceAction(#oauth_token = SecretValue(value = "None"), #action_name = "GitHub", #output = source_artifact, #trigger = codepipeline_actions.GitHubTrigger.POLL, ##Replace these with your actual GitHub project info# owner = "aws-samples", #repo = "faropt"),
+        pipeline = CdkPipeline(self, "Pipeline",
+            pipeline_name = "MyAppPipeline",
+            cloud_assembly_artifact = cloud_assembly_artifact,
+            source_action = codepipeline_actions.S3SourceAction(
+                bucket = bucket.bucket_name,
+                bucket_key = "faropt-master.zip",
+                action_name = "S3",
+                output = source_artifact),
+            synth_action = SimpleSynthAction.standard_npm_synth(
+                source_artifact = source_artifact,
+                cloud_assembly_artifact = cloud_assembly_artifact,
+                build_command = "cdk synth")
+        )
